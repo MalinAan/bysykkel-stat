7f26774d9c532d70a404c18c2a1a819c7a3ca1ba
Malin Aandahl <malinaandahl@gmail.com>
2021-04-24 12:32:58 +0200

    Third version

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import './App.css';
 import data from "../src/trips.json";
+import logo from "./bysykkel-logo.svg";
 import React from 'react';
+import { ResponsivePie } from '@nivo/pie'
+import { ResponsiveLine } from '@nivo/line'
+import {Line, Pie} from "recharts";
 
 
 const norwegianMonths = ["Januar", "Februar", "Mars", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Desember"];
+
+
 
 function App() {
 
@@ -15,25 +21,28 @@ function App() {
     // Hadde vært kult med noe visuelt på antall stasjoner.
 
     const numberOfTrips = data.length
+    interface MonthData {
+        monthNumber: number;
+        monthName: string;
+        year: number;
+        trips: number;
+        date: Date;
+    }
 
     let years: { [key: string]: number } = {};
     let months: { [key: string]: number } = {};
     let stations: { [key: string]: number } = {};
+    let monthsData:  { [key: string]: MonthData } = {};
+    interface PieData {
+        id: string;
+        value: number;
+        label: string;
+    }
 
-
-    data.forEach(trip => {
-        const date = new Date(trip._tripStarted)
-        const year = date.getFullYear().toString();
-        const month = norwegianMonths[date.getMonth()] + " " + year;
-        const startStation = trip._startStation._title;
-        const endStation = trip._endStation ? trip._endStation._title : trip._startStation._title;
-
-        addToDictionary(years, year);
-        addToDictionary(months, month);
-        addToDictionary(stations, startStation);
-        addToDictionary(stations, endStation);
-
-    })
+    interface LineChartData {
+        x: string; // Month
+        y: number; // Trips
+    }
 
     function addToDictionary(dictionary: { [key: string]: number }, key: string | number) {
         const entryYear = dictionary[key];
@@ -44,6 +53,69 @@ function App() {
         }
     }
 
+
+    data.forEach(trip => {
+        const date = new Date(trip._tripStarted)
+        const year = date.getFullYear().toString();
+        const month = norwegianMonths[date.getMonth()] + " " + year;
+        const mmyyyy = date.getMonth() + " " + year;
+        const startStation = trip._startStation._title;
+        const endStation = trip._endStation ? trip._endStation._title : trip._startStation._title;
+
+        addToDictionary(years, year);
+        addToDictionary(months, month);
+        addToDictionary(stations, startStation);
+        addToDictionary(stations, endStation);
+
+        const entryMonth = monthsData[mmyyyy];
+        if (entryMonth) {
+            monthsData[mmyyyy].trips = monthsData[mmyyyy].trips + 1;
+        } else {
+            monthsData[mmyyyy] = {
+                year: date.getFullYear(),
+                monthName: norwegianMonths[date.getMonth()],
+                trips: 1,
+                monthNumber: date.getMonth() + 1,
+                date: date
+            }
+
+        }
+
+    })
+
+    const pies = Object.entries(years).map(([year, numberOfTrips]) => {
+        const piePart:PieData =
+            {
+                label:year,
+                id:year,
+                value:numberOfTrips
+            }
+            return piePart;
+        }
+    )
+    interface LinesDataToYears {
+        id: string;
+        color: string;
+        data: LineChartData[]
+    }
+
+    let monthsIntoYears:LinesDataToYears[] = [];
+    const linesData = Object.entries(monthsData).map(([monthKey, monthData]) => {
+            const lineMonth:LineChartData =
+                {
+                    x:monthData.monthNumber + " " + monthData.year,
+                    y:monthData.trips
+                }
+            return lineMonth;
+        }
+    )
+
+    const allLines = [{"id": "alle-turer", "color": "red", "data": linesData}]
+
+    // Tenk litt på hvordan det skal struktures.
+
+
+
     const monthsSortedDescendingTrips = Object.entries(months)
         .sort(([month1, numberOfTripsPerMonth1], [month2, numberOfTripsPerMonth2]) => numberOfTripsPerMonth2 - numberOfTripsPerMonth1)
 
@@ -52,33 +124,11 @@ function App() {
 
     return (
         <div className="App">
+
             <div className="container">
                 <div className="header">
-                    <div className="header-logo">
-            <svg viewBox="0 0 33 24" width="347" height="347" xmlns="http://www.w3.org/2000/svg">
-                <defs>
-                    <path id="a" d="M14.963.13v15.31H.01V.13h14.953z"></path>
-                    <path id="c" d="M.026.141h12.5v4.465H.026V.14z"></path>
-                </defs>
-                <g fill="none" fill-rule="evenodd">
-                    <g transform="translate(17.919 8.42)">
-                        <mask id="b" fill="#fff"></mask>
-                        <path
-                            d="M7.487 3.659c-2.223 0-4.03 1.85-4.03 4.126 0 2.275 1.807 4.126 4.03 4.126 2.222 0 4.03-1.85 4.03-4.126 0-2.275-1.808-4.126-4.03-4.126m0 11.78C3.364 15.44.01 12.006.01 7.785.01 3.565 3.364.131 7.487.131c4.122 0 7.476 3.433 7.476 7.654 0 4.22-3.354 7.654-7.476 7.654"
-                            fill="currentColor"></path>
-                    </g>
-                    <path
-                        d="M7.477 12.078c-2.222 0-4.03 1.85-4.03 4.126 0 2.275 1.808 4.126 4.03 4.126 2.223 0 4.03-1.85 4.03-4.126 0-2.275-1.807-4.126-4.03-4.126m0 11.78C3.354 23.859 0 20.426 0 16.205c0-4.22 3.354-7.655 7.477-7.655s7.477 3.434 7.477 7.655c0 4.22-3.354 7.655-7.477 7.655M13.033 6.534H7.006c-.956 0-1.73-.793-1.73-1.771 0-.978.774-1.771 1.73-1.771h6.027c.956 0 1.73.793 1.73 1.77 0 .979-.774 1.772-1.73 1.772"
-                        fill="currentColor"></path>
-                    <g transform="translate(17.321 .011)">
-                        <mask id="d" fill="#fff">
-                        </mask>
-                        <path
-                            d="M6.261 4.606a13.07 13.07 0 0 1-5.177-1.06C.206 3.17-.207 2.136.16 1.237A1.71 1.71 0 0 1 2.415.291a9.71 9.71 0 0 0 3.846.787A9.713 9.713 0 0 0 10.133.28a1.709 1.709 0 0 1 2.257.94c.37.898-.041 1.933-.918 2.312a13.072 13.072 0 0 1-5.21 1.074"
-                            fill="currentColor" ></path>
-                    </g>
-                </g>
-            </svg>
+                    <div>
+                        <img className="header-logo" src={logo}/>
                     </div>
                     <div className="header-text">
                         <h1>Statistikk</h1>
@@ -86,27 +136,90 @@ function App() {
 
                 </div>
 
-                <div className="info-element">
-                    <h2> Antall turer: {numberOfTrips}</h2>
-                    <tbody>
-
-                    <table className="styled-table">
-                        <tr>
-                            <th>År</th>
-                            <th>Antall turer</th>
-                        </tr>
-                        {Object.entries(years).map(([year, numberOfTripsPerYear]) => (
-                            <tr key={year}>
-                                <td>{year}</td>
-                                <td>{numberOfTripsPerYear}</td>
-                            </tr>
-                        ))}
-                    </table>
-                    </tbody>
+                <div className="trips-element">
+                    <div className="trips-text">
+                    <h2 className="non-coloured"> Du har syklet totalt</h2>
+                    <h2 className="coloured"> {numberOfTrips} turer</h2>
+                    </div>
+                    <div className="pie-element">
+                        <ResponsivePie
+                            data={pies}
+                            margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+                            innerRadius={0.5}
+                            padAngle={0.7}
+                            cornerRadius={3}
+                            activeOuterRadiusOffset={8}
+                            colors={{ scheme: 'reds' }}
+                            borderWidth={1}
+                            borderColor={{ from: 'color', modifiers: [ [ 'darker', 0 ] ] }}
+                            arcLinkLabelsSkipAngle={10}
+                            arcLinkLabelsTextColor="#333333"
+                            arcLinkLabelsThickness={2}
+                            arcLinkLabelsColor={{ from: 'color' }}
+                            arcLabelsTextColor={{ from: 'color', modifiers: [ [ 'darker', 2 ] ] }}
+                            isInteractive={true}
+                            legends={[]}
+                        />
+                    </div>
                 </div>
                 <div className="info-element">
                     <h3> Mest populære måned var {monthsSortedDescendingTrips[0][0]},
                         med {monthsSortedDescendingTrips[0][1]} turer! </h3>
+                    <div className="line-chart-element">
+                        <ResponsiveLine
+                            data={allLines}
+                            margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+                            xScale={{ type: 'point' }}
+                            yScale={{ type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false }}
+                            yFormat=" >-.2f"
+                            curve="natural"
+                            axisTop={null}
+                            axisRight={null}
+                            axisBottom={{
+                                orient: 'bottom',
+                                tickSize: 4,
+                                tickPadding: 4,
+                                tickRotation: 0,
+                                legend: '',
+                                legendOffset: -60,
+                                legendPosition: 'middle'
+                            }}
+                            axisLeft={{
+                                orient: 'left',
+                                tickSize: 5,
+                                tickPadding: 5,
+                                tickRotation: 0,
+                                legend: 'Antall turer',
+                                legendOffset: -40,
+                                legendPosition: 'middle'
+                            }}
+                            enableGridX={false}
+                            enableGridY={false}
+                            colors={{ scheme: 'reds' }}
+                            enablePoints={false}
+                            pointSize={10}
+                            pointColor={{ theme: 'background' }}
+                            pointBorderWidth={2}
+                            pointBorderColor={{ from: 'serieColor', modifiers: [] }}
+                            pointLabelYOffset={-12}
+                            areaOpacity={0.15}
+                            isInteractive={false}
+                            legends={[{
+                                anchor: 'bottom-right',
+                                direction: 'column',
+                                justify: false,
+                                translateX: 100,
+                                translateY: 0,
+                                itemsSpacing: 0,
+                                itemDirection: 'left-to-right',
+                                itemWidth: 80,
+                                itemHeight: 20,
+                                itemOpacity: 0.75,
+                                symbolSize: 12,
+                                symbolShape: 'circle',
+                                symbolBorderColor: 'rgba(0, 0, 0, .5)',}]}
+                        />
+                    </div>
                     <tbody>
                     <table className="styled-table">
                         <tr>
