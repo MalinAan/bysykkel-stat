2a49442928e4ee84ed3fdad841f14f2e518d13e9
Malin Aandahl <malinaandahl@gmail.com>
2021-04-24 22:18:47 +0200

    Refactor

--- src/PieChart.tsx
+++ src/PieChart.tsx
@@ -1,10 +1,6 @@
 import './App.css';
-import data from "../src/trips.json";
-import logo from "./bysykkel-logo.svg";
-import bike from "./bike.svg";
 import React from 'react';
-import { ResponsivePie } from '@nivo/pie'
-import { ResponsiveLine } from '@nivo/line'
+import {ResponsivePie} from '@nivo/pie'
 
 interface Props {
     years: { [key: string]: number };
@@ -18,11 +14,11 @@ interface PieData {
 
 const PieChart = (props: Props) => {
     const pies = Object.entries(props.years).map(([year, numberOfTrips]) => {
-            const piePart:PieData =
+            const piePart: PieData =
                 {
-                    label:year,
-                    id:year,
-                    value:numberOfTrips
+                    label: year,
+                    id: year,
+                    value: numberOfTrips
                 }
             return piePart;
         }
@@ -31,19 +27,19 @@ const PieChart = (props: Props) => {
         <div className="pie-element">
             <ResponsivePie
                 data={pies}
-                margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+                margin={{top: 40, right: 80, bottom: 80, left: 80}}
                 innerRadius={0.5}
                 padAngle={0.7}
                 cornerRadius={3}
                 activeOuterRadiusOffset={8}
-                colors={{ scheme: 'red_yellow_green' }}
+                colors={{scheme: 'red_yellow_green'}}
                 borderWidth={1}
-                borderColor={{ from: 'color', modifiers: [ [ 'darker', 0 ] ] }}
+                borderColor={{from: 'color', modifiers: [['darker', 0]]}}
                 arcLinkLabelsSkipAngle={10}
                 arcLinkLabelsTextColor="#333333"
                 arcLinkLabelsThickness={2}
-                arcLinkLabelsColor={{ from: 'color' }}
-                arcLabelsTextColor={{ from: 'color', modifiers: [ [ 'darker', 2 ] ] }}
+                arcLinkLabelsColor={{from: 'color'}}
+                arcLabelsTextColor={{from: 'color', modifiers: [['darker', 2]]}}
                 isInteractive={true}
                 legends={[]}
             />
