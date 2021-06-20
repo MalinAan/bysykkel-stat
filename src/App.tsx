efe696fc1ac18ffd4a1e09f60a27d787dd24c82f
Malin Aandahl <malinaandahl@gmail.com>
2021-06-20 20:54:38 +0200

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,11 @@ import Statistics from "./Statistics";
 import {useDropzone} from 'react-dropzone'
 import Header from "./Header";
 import Footer from "./Footer";
+import {Link, Route, useHistory, Switch} from "react-router-dom";
 
 function App() {
+    const history = useHistory();
     const [tripsData, setTripsData] = useState(data);
-    const [showStatistics, setShowStatistics] = useState(false);
     const onDrop = useCallback(acceptedFiles => {
         const reader = new FileReader();
         reader.onload = () => {
@@ -16,47 +17,53 @@ function App() {
             if (typeof (textResult) === "string") {
                 const jsonFile = JSON.parse(textResult);
                 setTripsData(jsonFile);
-                setShowStatistics(true);
+                history.push('/statistikk');
             }
         };
         reader.readAsText(acceptedFiles[0])
     }, [])
     const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
     return (
-        <div>
+        <>
             <Header/>
-            {!showStatistics && <div className="pre-page">
-                <div className="download-data-guide">
-                    <h3>Last opp din data</h3>
-                    <ol>
-                        <li>Logg inn med din bruker på oslobysykkel.no.</li>
-                        <li>Trykk på “Dine data”.</li>
-                        <li>Trykk på “Lag nye filer”.</li>
-                        <li>Trykk på “Lag JSON-filer”.</li>
-                        <li>Når den er klar, trykk “LAST NED FIL”.</li>
-                        <li>Åpne zip-filen.</li>
-                        <li>Last opp kun trips.json.</li>
-                    </ol>
-                    <div {...getRootProps()}>
-                        <input {...getInputProps()} />
-                        {
-                            isDragActive ?
-                                <p>Dra trip.json hit</p> :
-                                <button>Last opp trips.json</button>
-                        }
-                    </div>
-                </div>
-                <div className="example-stat">
-                    <h3>... eller se eksempel på statistikk her</h3>
-                    <button onClick={
-                        () => setShowStatistics(true)
-                    }>Se eksempelstatistikk</button>
-                </div>
+            <Switch>
+                <Route path="/statistikk">
+                    <Statistics tripsData={tripsData}/>
+                </Route>
+                <Route>
+                    <div className="pre-page">
+                        <div className="download-data-guide">
+                            <h3>Last opp din data</h3>
+                            <ol>
+                                <li>Logg inn med din bruker på oslobysykkel.no.</li>
+                                <li>Trykk på “Dine data”.</li>
+                                <li>Trykk på “Lag nye filer”.</li>
+                                <li>Trykk på “Lag JSON-filer”.</li>
+                                <li>Når den er klar, trykk “LAST NED FIL”.</li>
+                                <li>Åpne zip-filen.</li>
+                                <li>Last opp kun trips.json.</li>
+                            </ol>
+                            <div {...getRootProps()}>
+                                <input {...getInputProps()} />
+                                {
+                                    isDragActive ?
+                                        <p>Dra trip.json hit</p> :
+                                        <button>Last opp trips.json</button>
+                                }
+                            </div>
+                        </div>
+                        <div className="example-stat">
+                            <h3>... eller se eksempel på statistikk her</h3>
+                            <Link to="/statistikk">
+                                <button>Se eksempelstatistikk</button>
+                            </Link>
+                        </div>
 
-            </div>}
-            {showStatistics && <Statistics tripsData={tripsData}/>}
+                    </div>
+                </Route>
+            </Switch>
             <Footer/>
-        </div>
+        </>
     );
 }
 
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,14 +1,17 @@
 import logo from "./svg/bysykkel-logo.svg";
 import React from "react";
+import {Link} from "react-router-dom"
+
 const Header = () => {
     return (
-<div className="header">
-    <div>
-        <img className="header-logo" src={logo} alt="Header logo"/>
-    </div>
-    <div className="header-text">
-        <h1>Statistikk</h1>
-    </div>
-</div>)}
+        <Link to="/" className="header">
+            <div>
+                <img className="header-logo" src={logo} alt="Header logo"/>
+            </div>
+            <div className="header-text">
+                <h1>Statistikk</h1>
+            </div>
+        </Link>)
+}
 
 export default Header;
